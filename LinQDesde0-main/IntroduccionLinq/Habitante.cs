1f03fc49537d317502e505013037e2feff443e09
Karlx28 <mo0436032023@unab.edu.sv>
2024-09-05 22:18:56 -0600

    Comentando "Habitante"

--- LinQDesde0-main/IntroduccionLinq/Habitante.cs
+++ LinQDesde0-main/IntroduccionLinq/Habitante.cs
@@ -6,15 +6,26 @@ using System.Threading.Tasks;
 
 namespace IntroduccionLinq
 {
+    // Definición de la clase Habitante que representa a una persona que vive en una casa
     public class Habitante
     {
+
+        // Propiedad para almacenar el identificador único del habitante
         public int IdHabitante {get;set;}
+
+        // Propiedad para almacenar el nombre del habitante
         public string Nombre {get; set;}
+
+        // Propiedad que guarda la edad del habitante
         public int Edad {get; set;}
+
+        // Propiedad que almacena el Id de la casa en la que vive el habitante
         public int IdCasa { get; set;}
 
+        // Método que devuelve una cadena con los datos formateados del habitante
         public string datosHabitante() {
 
+            // Retorna una cadena con el nombre, edad y la referencia a la casa (IdCasa)
             return $"Soy {Nombre} con edad de {Edad} años vividos en {IdCasa}";
         }
     }
