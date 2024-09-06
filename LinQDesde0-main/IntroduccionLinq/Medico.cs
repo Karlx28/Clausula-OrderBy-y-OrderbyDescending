0788a6b80a5a193dae4158def7453de1ff8767c8
Karlx28 <mo0436032023@unab.edu.sv>
2024-09-05 22:20:29 -0600

    Comentando "Medico"

--- LinQDesde0-main/IntroduccionLinq/Medico.cs
+++ LinQDesde0-main/IntroduccionLinq/Medico.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace IntroduccionLinq
 {
+    // La clase Medico hereda de la clase Empleado, indicando que un médico es un tipo de empleado
     public class Medico : Empleado
     {
+        // Propiedad para almacenar el nombre del médico, que vuelve a definirse aquí
         public string nombre { get; set; }
     }
 }
